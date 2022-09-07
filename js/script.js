8fbd9a6dc8c3173e0137a3465b5d7e66509c0716
dwaynek24 <devinwayneknotts@gmail.com>
2022-09-07 15:43:03 -0700

    342 wedsday word randomizer complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 console.log("hi")
+const words= ["Astronomer","Equinox","Meteorite","Starburst", "Telescope" ]
+var rand = words[Math.floor(Math.random() * words.length)];
+
+
+
+const wordToGuess = rand;
+console.log(wordToGuess)
