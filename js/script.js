987c96d6155d54ab68ab88cb92e27f99c9b564ed
dwaynek24 <devinwayneknotts@gmail.com>
2022-09-08 15:27:32 -0700

    post erase

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-console.log("hi")
 const words= ["Astronomer","Equinox","Meteorite","Starburst", "Telescope" ]
 var rand = words[Math.floor(Math.random() * words.length)];
-
-
-
 const wordToGuess = rand;
+const hiddenWord =[];
+
+
+let genHiddenWord = () => {
+for(let i = 0; i < rand.length;i++){
+    hiddenWord.push("_");
+}
+return hiddenWord    
+}
+console.log(hiddenWord)
+
+
+
+genHiddenWord()
+
+console.log(hiddenWord)
 console.log(wordToGuess)
+const letterGuess = document.querySelector(".letterGuess")
+document.querySelector("#wordShowcase").innerHTML =hiddenWord;
+const logGuess = document.querySelector("#submitGuess")
+logGuess.addEventListener("click", function() {
+    
+        console.log(letterGuess)
+});
