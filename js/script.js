2498a0eaa1f29c5701ce1993b5ae1aa6e6cc134e
dwaynek24 <devinwayneknotts@gmail.com>
2022-09-11 12:35:44 -0700

    closer

--- js/script.js
+++ js/script.js
@@ -1,59 +1,80 @@
-
-
-const words= ["astronomer","equinox","meteorite","starburst", "telescope" ]
+const words = ["astronomer", "equinox", "meteorite", "starburst", "telescope"];
 const wordToGuess = rand;
-const wrongLetterBox = document.querySelector("wrongLetters")
-const submitButton = document.querySelector("#sub")
-const input = document.querySelector("#inputBox")
-const hiddenWord =[];
+const wrongLetterBox = document.querySelector("wrongLetters");
+const submitButton = document.querySelector("#sub");
+const input = document.querySelector("#inputBox");
+const hiddenWord = [];
 const guessedLetter = [];
-const wrongGuessCount= document.querySelector(".guessCount")
+const wrongGuessCount = document.querySelector(".guessCount");
 const correctLetters = [];
 const wrongLetters = [];
-let guess = [];
+const wordShow = document.querySelector("#wordShowcase");
+let guess = "";
 var rand = words[Math.floor(Math.random() * words.length)];
 
-
- let genHiddenWord = () => {
-    for(let i = 0; i < rand.length;i++){
-        hiddenWord.push("_");
+let genHiddenWord = () => {
+    for (let i = 0; i < rand.length; i++) {
+      hiddenWord.push("_");
+    
     }
-    return hiddenWord ;{
-        
-    }   
-    }
-    
-    let compareGuess = () => {
-        for(let i = 0; i < rand.length;i++) {
-      if  (rand.includes(guess)){
-        correctLetters.push(guess);
-      }
-      else {wrongLetters.push(guess)} {document.querySelector("#wrongLetters").innerHTML =wrongLetters;}
-    }}
-
-    genHiddenWord()
-    document.querySelector("#wordShowcase").innerHTML =hiddenWord;
-
-
-    
-    
-    submitButton.addEventListener("click", function() {
-        
-         guess=input.value
-         return guess, 
-         console.log(guess),
-         compareGuess()
-     })  
-    
-     
-
-     console.log(guess)
+  
+    hiddenWord.join("");
     console.log(hiddenWord)
-    console.log(rand)
-    // look up string.includes
+    wordShow.innerHTML=hiddenWord
+    
+   console.log(hiddenWord)
+  
+  //   return hiddenWord;
+    
+    
+  };
+  genHiddenWord();
+
+
+let compareGuess = () => {
+  // for(let i = 0; i < rand.length;i++) {
+  if (rand.includes(guess)) {
+    correctGuess();
+   
+    // hiddenWord.push(guess);
+    correctLetters.push(guess);
+  } else {
+    wrongLetters.push(guess);
+  }
+  document.querySelector("#wrongLetters").innerHTML = wrongLetters;
+
+  // }
+  console.log(correctLetters);
+  console.log(wrongLetters);
+};
+
+
+submitButton.addEventListener("click", function () {
+  guess = input.value;
+  compareGuess();
+  
+  
+  return guess, console.log(guess)
+});
+let correctGuess = () => {
+    // loop through rand if for loop conditional if ran index i = guess then replace
+    for (let i=0; i <rand.length;i++) {
+        if (rand[i]== guess){
+            hiddenWord.innerHTML= guess
+        }
+        console.log(hiddenWord)
+    }
+    
+    
+}
+
+
+console.log(guess);
+
+console.log(rand);
+// look up string.includes
 //  a check to see if string includes or does not
 // if it does include, replace that in hidden word array
 // if it doesnt include place letter wrong letter array
 // call render
-// replace wrong guesses 
-
+// replace wrong guesses
