06002cd0e24a4d950c5e36c90310da94a4fdfaa1
dwaynek24 <devinwayneknotts@gmail.com>
2022-09-12 11:04:01 -0700

    SEP12 1103

--- js/script.js
+++ js/script.js
@@ -11,89 +11,66 @@ const wrongLetters = [];
 const wordShow = document.querySelector("#wordShowcase");
 let guess = "";
 var rand = words[Math.floor(Math.random() * words.length)];
-var renderV =  hiddenWord.join("");
+var renderV = hiddenWord.join("");
 let genHiddenWord = () => {
-    for (let i = 0; i < rand.length; i++) {
-      hiddenWord.push("_");
-    
-    }
-    renderWord()
-  
-   
-    
-   
-  
-  //   return hiddenWord;
-    
-    
-  };
-
-  let renderWord = () => {
-   renderV= hiddenWord.join("")
-    
-    wordShow.innerHTML=hiddenWord
+  for (let i = 0; i < rand.length; i++) {
+    hiddenWord.push("_");
   }
-  genHiddenWord();
+  renderWord();
+
+  
+};
+
+let renderWord = () => {
+  renderV = hiddenWord.join("");
+
+  wordShow.innerHTML = hiddenWord;
+};
+genHiddenWord();
 
 
 let compareGuess = () => {
-  if (wrongLetters.length===2) {
-  loser()}
-  else if(hiddenWord.includes("_")===false){
-    winner()
-  }
-  else if (rand.includes(guess)) {
-    console.log("yay")
-    correctLetters.push(guess)
+  if (wrongLetters.length === 2) {
+    loser();
+  } else if (hiddenWord.includes("_") === false) {
+    winner();
+  } else if (rand.includes(guess)) {
+    correctLetters.push(guess);
     correctGuess();
-   
-    
-    renderWord()
+
+    renderWord();
   } else {
     wrongLetters.push(guess);
   }
   document.querySelector("#wrongLetters").innerHTML = wrongLetters;
-
-  // }
-  console.log(correctLetters);
-  console.log(wrongLetters);
 };
 
 
 submitButton.addEventListener("click", function () {
   guess = input.value;
   compareGuess();
-  console.log(hiddenWord);
-    
-  
-  
-  return guess
+
+  return guess;
 });
 let correctGuess = () => {
-    // loop through rand if for loop conditional if ran index i = guess then replace
-    for (let i=0; i <rand.length;i++) {
-        if (rand[i]== guess){
-            hiddenWord[i]=guess
-        }
-        // console.log(hiddenWord)
+  for (let i = 0; i < rand.length; i++) {
+    if (rand[i] == guess) {
+      hiddenWord[i] = guess;
     }
-    
-    
-}
+  }
+};
 let loser = () => {
-    {alert("*explosion... dont quit your day job Spacecowboy..refresh to try again")
-}}
+  {
+    alert(
+      "*explosion... dont quit your day job Spacecowboy..refresh to try again"
+    );
+  }
+};
 let winner = () => {
-    {alert("Blast off was a success! see you later Spacecowboy..refresh to play again.")
-}}
+  {
+    alert(
+      "Blast off was a success! see you later Spacecowboy..refresh to play again."
+    );
+  }
+};
 
-console.log(guess);
-console.log(hiddenWord)
-console.log(rand);
-console.log(wordToGuess)
-// look up string.includes
-//  a check to see if string includes or does not
-// if it does include, replace that in hidden word array
-// if it doesnt include place letter wrong letter array
-// call render
-// replace wrong guesses
